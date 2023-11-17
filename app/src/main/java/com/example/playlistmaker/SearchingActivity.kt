d3020bd06c753eed34fc62ed3dd8ae97514f46fc
Sevlaria <137600329+Sevlaria@users.noreply.github.com>
2023-11-17 20:40:30 +0300

    спринт 10-17.11

--- app/src/main/java/com/example/playlistmaker/SearchingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchingActivity.kt
@@ -20,6 +20,7 @@ import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.security.PrivateKey
 
 
@@ -147,6 +148,7 @@ class SearchingActivity: AppCompatActivity() {
                  .load(model.artworkUrl100)
                  .placeholder(R.drawable.placeholder)
                  .centerCrop()
+                 .transform(RoundedCorners(8))
                  .into(artImageView)
         }
     }
